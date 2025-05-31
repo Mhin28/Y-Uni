b281a3ec1832f45d3cc0d8451485e3f939c42adc
NET_14_NguyenLeMinh <minhvb123456@gmail.com>
2025-05-31 14:10:27 +0700

    first

---
+++ y-Uni/Services/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
+
